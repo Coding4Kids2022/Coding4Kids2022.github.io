0329088a0d9427a7e2ac9904208e49d547667776
Lema111 <110460422+Lema111@users.noreply.github.com>
2022-08-03 11:57:23 +0200

    Add files via upload

---
+++ randomnumbers.js
@@ -0,0 +1,10 @@
+var min = 1;
+var max = 45;
+
+
+var Zahl1 = Math.round(Math.random() * (max - min)) + min;
+
+var Zahl2 = Math.round(Math.random() * (max - min)) + min;
+
+var Zahl3 = Math.round(Math.random() * (max - min)) + min;
+
